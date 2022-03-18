d7e3851b475e2d9f05622791e87138b5a0ea6b5b
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2022-03-19 03:09:23 +0530

    PP-48 | Updated environment

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,27 +5,14 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   // add this key to environment variables
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCD8urdGaWmeo-FCOHGaQ11ZoBdFU9Tz9Y",
-  authDomain: "nu-placement-portal.firebaseapp.com",
-  projectId: "nu-placement-portal",
-  storageBucket: "nu-placement-portal.appspot.com",
-  messagingSenderId: "628208538317",
-  appId: "1:628208538317:web:5e35fe769472c54a098f84",
-  measurementId: "G-CMKQMYD6G2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
