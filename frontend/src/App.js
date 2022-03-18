3a6ff7b1914a05a8fc7e365d9725f7d20c46a8a5
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2022-03-19 01:13:31 +0530

    PP-48 | Package updation and pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Skeleton from './Skeleton';
+import React from "react";
+import Signup from "./components/Signup";
+import { AuthProvider } from "./contexts/AuthContext";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+// import Dashboard from "./Dashboard";
+import Login from "./components/Login";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+// import PrivateRoute from "./PrivateRoute";
+// import ForgotPassword from "./ForgotPassword";
+// import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
-    <Skeleton/>
+    <>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            {/* <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
+            <Route path="/signup" component={Signup} />
+
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
   );
 }
 
