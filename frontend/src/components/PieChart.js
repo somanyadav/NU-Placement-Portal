a4e495fb6b6b130057e0256adcc94388a5edd7fe
mahakfaheem <mahak.faheem19@st.niituniversity.in>
2022-04-12 16:45:55 +0530

    PP-67 | Dashboard Screen

---
+++ frontend/src/components/PieChart.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Chart as ChartJS, CategoryScale ,LinearScale,BarElement,Title,ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
+import faker from 'faker';
+// import { faker } from '@faker-js/faker';
+
+ChartJS.register(CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+export const bardata = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+
+
+export default function PieChart() {
+
+  return (
+    <div style={{alignContent:"center"}}>
+  <div style={{height:"40rem",width:"40rem"}}>
+  <Pie data={data} />
+  </div>
+  <div>
+  <Bar options={options} data={bardata} />
+  </div>
+  </div>);
+}
