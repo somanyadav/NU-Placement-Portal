c5efba2be6e5c9b54c5b55b0d0ff5abba3547899
mahakfaheem <mahak.faheem19@st.niituniversity.in>
2022-05-13 20:10:57 +0530

    PP-73 | Dashboard cards

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import Skeleton from "./Skeleton";
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
+import "../App.css"
 
 //export default function Dashboard() {
 //return <Skeleton />;
@@ -46,29 +47,96 @@ const Dashboard = () => {
         </div>
         <div class="row mb-3">
           <div class="col-xl-3 col-sm-6 py-2">
-            <div class="bg-success text-white h-100">
+            <div class="card-class" >
               <div
-                class="card-body bg-success"
-                style={{ backgroundColor: "#57b960" }}
+                class="card-body card-style"
+                style={{ border:"1px",borderStyle:"solid",borderColor:"black", color:"#81181d" }}
               >
                 <div class="rotate">
-                  <i class="fa fa-user fa-3x"></i>
+                  <i class="fa fa-list fa-2x"></i>
                 </div>
                 <br></br>
-                <h6 class="text-uppercase">Students Selected</h6>
-                <h1 class="display-4">134</h1>
+                <h6 class="text">JDs PUBLISHED</h6>
+                <h1 class="display-6">134</h1>
               </div>
             </div>
           </div>
+
           <div class="col-xl-3 col-sm-6 py-2">
+            <div class="card-class" >
+              <div
+                class="card-body card-style"
+                style={{ border:"1px",borderStyle:"solid",borderColor:"black", color:"#81181d" }}
+              >
+                <div class="rotate">
+                  <i class="fa fa-user fa-2x"></i>
+                </div>
+                <br></br>
+                <h6 class="text-uppercase">Students Shortlisted</h6>
+                <h1 class="display-6">134</h1>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-sm-6 py-2">
+            <div class="card-class" >
+              <div
+                class="card-body card-style"
+                style={{ border:"1px",borderStyle:"solid",borderColor:"black", color:"#81181d" }}
+              >
+                <div class="rotate">
+                  <i class="fa fa-pen fa-2x"></i>
+                </div>
+                <br></br>
+                <h6 class="text">IP OFFERED </h6>
+                <h1 class="display-6">134</h1>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-sm-6 py-2">
+            <div class="card-class" >
+              <div
+                class="card-body card-style"
+                style={{ border:"1px",borderStyle:"solid",borderColor:"black", color:"#81181d" }}
+              >
+                <div class="rotate">
+                  <i class="fa fa-share fa-2x"></i>
+                </div>
+                <br></br>
+                <h6 class="text-uppercase">PPO Offered</h6>
+                <h1 class="display-6">134</h1>
+              </div>
+            </div>
+          </div>
+
+          {/* <div class="row mb-3">
+          <div class="col-xl-3 col-sm-6 py-2">
+            <div class="card-class" >
+              <div
+                class="card-body card-style"
+                style={{ border:"1px",borderStyle:"solid",borderColor:"black", color:"#81181d" }}
+              >
+                <div class="rotate">
+                  <i class="fa fa-user fa-2x"></i>
+                </div>
+                <br></br>
+                <h6 class="text-uppercase">Students Selected</h6>
+                <h1 class="display-6">134</h1>
+              </div>
+            </div>
+          </div>
+          </div> */}
+          
+          {/* <div class="col-xl-3">
             <div class="card text-white bg-danger h-100">
-              <div class="card-body bg-danger">
+              <div class="card-class">
                 <div class="rotate">
                   <i class="fa fa-list fa-3x"></i>
                 </div>
                 <br></br>
                 <h6 class="text-uppercase">JDs Published</h6>
-                <h1 class="display-4">87</h1>
+                <h1 class="display-6">87</h1>
               </div>
             </div>
           </div>
@@ -80,7 +148,7 @@ const Dashboard = () => {
                 </div>
                 <br></br>
                 <h6 class="text-uppercase">Offers Accepted</h6>
-                <h1 class="display-4">125</h1>
+                <h1 class="display-6">125</h1>
               </div>
             </div>
           </div>
@@ -92,10 +160,10 @@ const Dashboard = () => {
                 </div>
                 <br></br>
                 <h6 class="text-uppercase">PPO</h6>
-                <h1 class="display-4">36</h1>
+                <h1 class="display-6">36</h1>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <hr />
